97c34ec04b9cdec7ca1ace1f62ffad59ddc09151
pijawca <devpijawca@gmail.com>
2024-06-30 22:54:49 +0700

    commit

--- Адитья-Бхаргаева-Грокаем-алгаритмы/ch1.py
+++ Адитья-Бхаргаева-Грокаем-алгаритмы/ch1.py
@@ -1,62 +1,80 @@
-'''
-Имеется отсортированный список из 128 имен, и вы ищете в нем зна-
-чение методом бинраного поиска. Какое максимальное количество
-проверок для этого может потребоваться?
-'''
-
-def search(lst, item):
-    low = 0
-    high = len(lst) - 1
-    attempts = 0
+# '''1.1
+# Имеется отсортированный список из 128 имен, и вы ищете в нем зна-
+# чение методом бинраного поиска. Какое максимальное количество
+# проверок для этого может потребоваться?
+# '''
+# def search(lst, item):
+#     low = 0
+#     high = len(lst) - 1
+#     attempts = 0
     
-    while low <= high:
-        attempts += 1
-        mid = (low + high) // 2
-        guess = lst[mid]
+#     while low <= high:
+#         attempts += 1
+#         mid = (low + high) // 2
+#         guess = lst[mid]
         
-        print(f'Попытка {attempts} : индекс {mid} : значение {guess}')
+#         if guess == item + 1:
+#             print(f'Количество попыток: {attempts}')
+#             return mid
         
-        if guess == item + 1:
-            print(f'Количество попыток: {attempts}')
-            return mid
-        
-        if guess > item + 1:
-            high = mid - 1
-        else:
-            low = mid + 1
-            
-    return None
+#         if guess > item + 1:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return None
 
-my_list = list(range(1, 128))
-index = search(my_list, 64)
+# my_list = list(range(1, 128))
+# index = search(my_list, 64)
 
-'''
-Предположим, размер списка увеличился вдвое. Как изменится мак-
-симальное количество проверок?
-'''
 
-def search(lst, item):
-    low = 0
-    high = len(lst) - 1
-    attempts = 0
+# '''1.2
+# Предположим, размер списка увеличился вдвое. Как изменится мак-
+# симальное количество проверок?
+# '''
+
+# def search(lst, item):
+#     low = 0
+#     high = len(lst) - 1
+#     attempts = 0
     
-    while low <= high:
-        attempts += 1
-        mid = (low + high) // 2
-        guess = lst[mid]
-        
-        print(f'Попытка {attempts} : индекс {mid} : значение {guess}')
-        
-        if guess == item + 1:
-            print(f'Количество попыток: {attempts}')
-            return mid
-        
-        if guess > item + 1:
-            high = mid - 1
-        else:
-            low = mid + 1
-            
-    return None
+#     while low <= high:
+#         attempts += 1
+#         mid = (low + high) // 2
+#         guess = lst[mid]
 
-my_list = list(range(1, 256))
-index = search(my_list, 128)
+#         if guess == item + 1:
+#             print(f'Количество попыток: {attempts}')
+#             return mid
+        
+#         if guess > item + 1:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return None
+
+# my_list = list(range(1, 256))
+# index = search(my_list, 128)
+
+
+# '''1.3
+# Известна фамилия, нужно найти номер в телефонной книге.
+# ''' 
+# def searchingNumber():
+#     book = ['Иванов', 'Шмыков', 'Попов', 'Поротников', 'Быков', 'Гаянов', 'Марков', 'Носов', 'Носков', 'Глазов']
+#     second = 'Быков'
+#     try:
+#         if second in book:
+#             print(f'Была найдена фамилия {second}а')
+#         else:
+#             print('Такой фамилии нет')
+#     finally:
+#         pass
+#     return
+    
+# searchingNumber()
+
+
+'''1.4
+Известен номер, нужно найти фамилию в телефонной книге. (Под-
+сказка: вам придется провести поиск по все книге!)
+'''
